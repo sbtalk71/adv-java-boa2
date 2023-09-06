1125abcd838ff8a9c1fe2cd663c8ef3e278a13af
sbtalk71 <sbtalk@yahoo.com>
2023-09-06 16:13:39 +0530

    updated

--- day3/src/com/demo/streams/ex3/StreamsLab.java
+++ day3/src/com/demo/streams/ex3/StreamsLab.java
@@ -1,6 +1,8 @@
 package com.demo.streams.ex3;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class StreamsLab {
 
@@ -15,10 +17,19 @@ public class StreamsLab {
 		Student st4=
 				new Student(103,"Lekha",new Address("500079","Hyderabad"),Arrays.asList(new MobileNumber("222333"),new MobileNumber("445544")));
 
+		List<Student> studentsList=Arrays.asList(st1,st2,st3,st4);
 		//Find the first student whose name matches "Puja", if not found then print "not found"
+		
+		Optional<Student> student=studentsList.stream().filter(st->st.getName().equals("Puja")).findFirst();
+		System.out.println(student.isPresent()?student.get().getName():"Not Found");
+		
 		//List all the students with pincode 500079
 		//find the student whose mobile number is 123321
-		//
+		
+		Optional<Student> st5=studentsList.stream().filter(st->st.getMobileNumbers().stream().anyMatch(n->n.getNumber().equals("123321"))).findFirst();
+		
+		System.out.println(st5.isPresent()?st5.get().getName():"No Student found");
+		
 		
 	}
 
