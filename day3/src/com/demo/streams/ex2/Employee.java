482127e90279c22ae0a26f6500c8e5f2a128fe9d
sbtalk71 <sbtalk@yahoo.com>
2023-09-06 14:26:32 +0530

    updated data

---
+++ day3/src/com/demo/streams/ex2/Employee.java
@@ -0,0 +1,80 @@
+package com.demo.streams.ex2;
+
+import java.util.Objects;
+
+public class Employee {
+	private int empId;
+	private String name;
+	private String location;
+	private double salary;
+
+	public Employee() {
+
+	}
+
+	public Employee(int empId, String name, String location, double salary) {
+		this.empId = empId;
+		this.name = name;
+		this.location = location;
+		this.salary = salary;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "{"+empId+" "+name+"}";
+	}
+
+	@Override
+	public int hashCode() {
+		System.out.println("hashcode called..");
+		return Objects.hash(empId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("equals() called..");
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return empId == other.empId;
+	}
+	
+	
+	
+}
---
+++ day3/src/com/demo/streams/ex2/StreamsWithObjectsDemo.java
@@ -0,0 +1,26 @@
+package com.demo.streams.ex2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsWithObjectsDemo {
+
+	public static void main(String[] args) {
+		List<Employee> empList=Arrays.asList(
+
+				new Employee(100, "Ranganathan", "Chennai", 56000),
+				new Employee(101, "Pavan", "Hyderabad", 76000),
+				new Employee(102, "Raja", "Chennai", 78000),
+				new Employee(103, "Shivaraj", "Bangalore", 35000),
+				new Employee(104, "Arjuna", "Bangalore", 530000),
+				new Employee(105, "Karan", "Hyderabad", 430000),
+				new Employee(106, "Jeevan", "Chennai", 98000),
+				new Employee(107, "Shambhu", "Hyderabad", 46000),
+				new Employee(108, "Rahguram", "Chennai", 36000),
+				new Employee(109, "Charan", "Hyderabad", 86000)
+				
+				);
+
+	}
+
+}
